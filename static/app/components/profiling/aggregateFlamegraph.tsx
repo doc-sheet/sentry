ea47f25e6b3fc0e7004be013bb97ff34e15c7fec
Elias Hussary <elias.hussary@sentry.io>
2023-02-28 13:53:20 -0500

    wip

---
+++ static/app/components/profiling/aggregateFlamegraph.tsx
@@ -0,0 +1,37 @@
+import {normalizeDateTimeParams} from 'sentry/components/organizations/pageFilters/parse';
+import {useCurrentProjectFromRouteParam} from 'sentry/utils/profiling/hooks/useCurrentProjectFromRouteParam';
+import {useQuery} from 'sentry/utils/queryClient';
+import {MutableSearch} from 'sentry/utils/tokenizeSearch';
+import useOrganization from 'sentry/utils/useOrganization';
+import usePageFilters from 'sentry/utils/usePageFilters';
+
+export function AggregateFlamegraph({transaction}: {transaction: string}) {
+  const query = useAggregateFlamegraphQuery({transaction});
+  return <div>Flamegraph</div>;
+}
+
+function useAggregateFlamegraphQuery({transaction}: {transaction: string}) {
+  const {selection} = usePageFilters();
+  const organization = useOrganization();
+  const project = useCurrentProjectFromRouteParam();
+  const url = `/projects/${organization.slug}/${project?.slug}/profiling/flamegraph/`;
+  const conditions = new MutableSearch([]);
+  conditions.setFilterValues('transaction_name', [transaction]);
+
+  return useQuery(
+    [
+      url,
+      {
+        query: {
+          query: conditions.formatString(),
+          ...normalizeDateTimeParams(selection.datetime),
+        },
+      },
+    ],
+    {
+      staleTime: 0,
+      retry: false,
+      enabled: Boolean(organization?.slug && project?.slug && selection.datetime),
+    }
+  );
+}
